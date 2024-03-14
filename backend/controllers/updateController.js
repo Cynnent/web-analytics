ad3c0b917d144c70f4d6f6bb2f7d00ff84d3e2a5
prasathkumara <prasathkumar.a@cynnent.com>
2024-03-14 18:16:53 +0530

    latest backend changes

--- backend/controllers/updateController.js
+++ backend/controllers/updateController.js
@@ -1,32 +1,25 @@
-
-const mongoose = require('mongoose');
-const User = require('../models/userModel')
-
-
-// updateController.js
+const mongoose = require("mongoose");
+const User = require("../models/userModel");
 
 const updateData = async (req, res) => {
   const userId = req.params.userId;
-
   try {
-    // Find the user by ID
     const user = await User.findById(userId);
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     const clientPayload = req.body;
 
-    // Iterate through the payload
-    clientPayload.forEach(clientEvent => {
-      // Find the existing event with the same date
-      const existingEvent = user.userEvents.find(existingEvent => existingEvent.date === clientEvent.date);
+    clientPayload.forEach((clientEvent) => {
+      const existingEvent = user.userEvents.find(
+        (existingEvent) => existingEvent.date === clientEvent.date
+      );
 
       if (existingEvent) {
-        // Update existing user event
-        Object.keys(clientEvent).forEach(screenKey => {
-          if (screenKey !== 'date') {
+        Object.keys(clientEvent).forEach((screenKey) => {
+          if (screenKey !== "date") {
             if (!existingEvent.screens) {
               existingEvent.screens = {};
             }
@@ -45,14 +38,13 @@ const updateData = async (req, res) => {
           }
         });
       } else {
-        // Add new user event
         const newUserEvent = {
           date: clientEvent.date,
           screens: {},
         };
 
-        Object.keys(clientEvent).forEach(screenKey => {
-          if (screenKey !== 'date') {
+        Object.keys(clientEvent).forEach((screenKey) => {
+          if (screenKey !== "date") {
             newUserEvent.screens[screenKey] = { ...clientEvent[screenKey] };
           }
         });
@@ -60,78 +52,66 @@ const updateData = async (req, res) => {
         user.userEvents.push(newUserEvent);
       }
     });
-
-      // Calculate total count for each date and update 'totalCount' field
-      user.userEvents.forEach(userEvent => {
-        let totalCount = 0;
-        if (userEvent.screens) {
-          Object.keys(userEvent.screens).forEach(screenKey => {
-            Object.values(userEvent.screens[screenKey]).forEach(value => {
-              totalCount += value;
-            });
+    user.userEvents.forEach((userEvent) => {
+      let totalCount = 0;
+      if (userEvent.screens) {
+        Object.keys(userEvent.screens).forEach((screenKey) => {
+          Object.values(userEvent.screens[screenKey]).forEach((value) => {
+            totalCount += value;
           });
-        }
-        userEvent.totalCount = totalCount;
-      });
-
-    // Mark the 'userEvents' field as modified
-    user.markModified('userEvents');
+        });
+      }
+      userEvent.totalCount = totalCount;
+    });
+    user.markModified("userEvents");
 
     try {
-      // Save the updated user object
       await user.save();
-      console.log('User saved successfully');
     } catch (error) {
-      console.error('Error saving user:', error);
+      console.error("Error saving user:", error);
     }
 
-    res.json({ message: 'User events updated successfully', user });
+    res.json({ message: "User events updated successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-
-
 const user = async (req, res) => {
-    const userData = req.body;
-    const configData = {
-        endpoint: 'https://webanalyticals.onrender.com',
-        serverUpdateTime: 5000,
-        token: '',
-    };
+  const userData = req.body;
+  const configData = {
+      endpoint: 'https://webanalyticals.onrender.com',
+      serverUpdateTime: 5000,
+      token: '',
+  };
 
-    try {
-        const response = {
-            ...configData,
-            _id: undefined, // To exclude _id from the response initially
-        };
+  try {
+      const response = {
+          ...configData,
+          _id: undefined, 
+      };
 
-        const newUser = new User({ ...userData, ...configData });
-        const savedUser = await newUser.save();
+      const newUser = new User({ ...userData });
+      const savedUser = await newUser.save();
+      response._id = savedUser._id;
 
-        // Update the response with the generated _id
-        response._id = savedUser._id;
-
-        res.json(response);
-    } catch (error) {
-        console.error('Error creating config:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+      res.json(response);
+  } catch (error) {
+      console.error('Error creating config:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
 
-
-
-const getUsersData= async(req, res) => {
+const getUsersData = async (req, res) => {
   try {
     const users = await User.find();
 
     res.status(200).json(users);
   } catch (err) {
-    console.error('Error retrieving data from MongoDB:', err);
-    res.status(500).send('Internal Server Error');
+    console.error("Error retrieving data from MongoDB:", err);
+    res.status(500).send("Internal Server Error");
   }
-}
+};
 
-module.exports = {updateData, user, getUsersData}
+module.exports = { updateData, user, getUsersData };
